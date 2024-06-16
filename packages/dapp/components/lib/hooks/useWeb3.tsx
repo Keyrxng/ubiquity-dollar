f6ae8e06446902efff40ff521801220019b93fff
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-16 22:39:57 +0100

    chore: memo-ize web3State

--- packages/dapp/components/lib/hooks/useWeb3.tsx
+++ packages/dapp/components/lib/hooks/useWeb3.tsx
@@ -6,6 +6,7 @@ import { FC, useEffect, useState } from "react";
 import { ChildrenShim } from "./children-shim";
 // @ts-expect-error - no types
 import { RPCHandler } from "@ubiquity-dao/rpc-handler";
+import { useMemo } from "react";
 
 const IS_DEV = process.env.NEXT_PUBLIC_NODE_ENV === "development";
 
@@ -47,44 +48,101 @@ export const UseWeb3Provider: FC<ChildrenShim> = ({ children }) => {
   );
 };
 
-const useHandlerFastestRpc = (): JsonRpcProvider => {
+/**
+ * Singleton class to handle the RPC provider
+ * so that we supply one provider across the app
+ */
+class Handler {
+  provider_: JsonRpcProvider | null;
+  handler_: RPCHandler | null;
+  initializing_: Promise<void> | null;
+  static instance: Handler | null = null;
+  constructor() {
+    this.provider_ = null;
+    this.handler_ = new RPCHandler({
+      networkId: IS_DEV ? 31337 : 1,
+      autoStorage: true,
+      cacheRefreshCycles: 10,
+      rpcTimeout: 1500,
+      networkName: null,
+      runtimeRpcs: null,
+      networkRpcs: null,
+    });
+    this.initializing_ = null;
+    this.provider_ = null;
+  }
+
+  static getInstance(): Handler {
+    if (!Handler.instance) {
+      Handler.instance = new Handler();
+    }
+    return Handler.instance;
+  }
+
+  async init() {
+    if (!this.provider_ && !this.initializing_) {
+      this.initializing_ = (async () => {
+        const provider = await this.handler_.getFastestRpcProvider();
+        this.provider_ = provider;
+        this.initializing_ = null;
+      })();
+    }
+    if (this.initializing_) {
+      await this.initializing_;
+    }
+
+    return this.provider_;
+  }
+
+  get handler() {
+    return this.handler_;
+  }
+
+  get provider() {
+    if (!this.provider_) {
+      this.init();
+    }
+    return this.provider_;
+  }
+
+  set handler(handler: RPCHandler | null) {
+    this.handler_ = handler;
+  }
+
+  set provider(provider: JsonRpcProvider | null) {
+    this.provider_ = provider;
+  }
+}
+
+const useHandlerFastestRpc = () => {
   const [provider, setProvider] = useState<JsonRpcProvider | null>(null);
 
-  const handler = new RPCHandler({
-    networkId: IS_DEV ? 31337 : 1,
-    autoStorage: true,
-    cacheRefreshCycles: 10,
-    rpcTimeout: 1500,
-    networkName: null,
-    runtimeRpcs: null,
-    networkRpcs: null,
-  });
-
   useEffect(() => {
-    async function getProvider() {
-      const provider = await handler.getFastestRpcProvider();
-      setProvider(provider);
-    }
-    getProvider();
+    const handlerInstance = Handler.getInstance();
+    handlerInstance.init().then(() => {
+      setProvider(handlerInstance.provider);
+    });
   }, []);
 
-  return provider as JsonRpcProvider;
+  return useMemo(() => ({ provider }), [provider]);
 };
 
 const useWeb3 = (): [Web3State] => {
   const { isConnecting, address } = useAccount();
   const { data: signer } = useSigner();
-  const provider = useHandlerFastestRpc();
+  const { provider } = useHandlerFastestRpc();
 
-  const web3State = {
-    metamaskInstalled,
-    jsonRpcEnabled: IS_DEV,
-    providerMode: "none" as "none" | "metamask" | "jsonrpc",
-    provider: provider as PossibleProviders,
-    connecting: isConnecting,
-    walletAddress: address as string,
-    signer: signer as JsonRpcSigner,
-  };
+  const web3State = useMemo(() => {
+    return {
+      metamaskInstalled,
+      jsonRpcEnabled: IS_DEV,
+      providerMode: "none" as "none" | "metamask" | "jsonrpc",
+      provider: provider,
+      connecting: isConnecting,
+      walletAddress: address as string,
+      signer: signer as JsonRpcSigner,
+    };
+  }, [provider, signer, address, isConnecting]);
 
   return [web3State];
 };
